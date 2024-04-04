ad541ab55d91a7cf7c5d2fcbe4a2f33a05649039
Clement BOUDINEL <clement.boudinel@lesgrappes.com>
2024-04-04 21:59:30 +0200

    implement a basic list of my recipes & a detailed view of a recipe

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Repository\CommentRepository;
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'app_recipe')]
-    public function index(): Response
+    #[Route('/recipes', name: 'recipes')]
+    public function index(RecipeRepository $recipeRepository): Response
     {
         return $this->render('recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+            'recipes' => $recipeRepository->findAll()
+        ]);
+    }
+
+    #[Route('/recipes/{id}', name: 'recipe')]
+    public function show(Request $request, Recipe $recipe, CommentRepository $commentRepository): Response
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($recipe, $offset);
+
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
         ]);
     }
 }
