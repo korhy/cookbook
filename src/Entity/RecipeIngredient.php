396064e6e329f4092894046da2ca0cededfe232d
Clement BOUDINEL <clementboudinel@gmail.com>
2026-02-01 18:48:40 +0100

    feat(entity): add Ingredient and RecipeIngredient entities with repositories

---
+++ src/Entity/RecipeIngredient.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeIngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeIngredientRepository::class)]
+class RecipeIngredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $quantity = null;
+
+    #[ORM\ManyToOne(inversedBy: 'recipeIngredients')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Recipe $recipe = null;
+
+    #[ORM\ManyToOne(inversedBy: 'recipeIngredients')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Ingredient $ingredient = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuantity(): ?float
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(?float $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getRecipe(): ?Recipe
+    {
+        return $this->recipe;
+    }
+
+    public function setRecipe(?Recipe $recipe): static
+    {
+        $this->recipe = $recipe;
+
+        return $this;
+    }
+
+    public function getIngredient(): ?Ingredient
+    {
+        return $this->ingredient;
+    }
+
+    public function setIngredient(?Ingredient $ingredient): static
+    {
+        $this->ingredient = $ingredient;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->ingredient ? $this->ingredient->getId() : '';
+    }
+}
